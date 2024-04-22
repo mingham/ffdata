f426487f08ba06330a6a37ae18f9437d2a8407a0
mingham <ingham.mark77@gmail.com>
2024-04-22 18:34:16 +0100

    ammended intro page

--- src/app/About.py
+++ src/app/About.py
@@ -9,6 +9,9 @@ st.markdown("""
             # Welcome to FF Data App
             ## Various tools to examine Prof Kenneth French's Data Sets
             ## See the left sidebar for available tools
+
+            You can use the filters in the sidebar to select 3 or 5 factor returns, 
+            industry returns and most combos that are available on [Prof Ken French's website]
             """)
 
 
